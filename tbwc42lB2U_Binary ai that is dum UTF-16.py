20e0e9b27b7f8476c98597da11f8a894cfcd70fe
veothm <116318783+veothm@users.noreply.github.com>
2023-05-18 10:21:31 -0400

    Add files via upload
    use https://www.rapidtables.com/convert/number/binary-to-ascii.html and UTF-16

---
+++ tbwc42lB2U_Binary ai that is dum UTF-16.py
@@ -0,0 +1,385 @@
+#china tadisnel
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
+import random
+num = random.random()
+if num>0.5:
+    var=0
+    print(var)
+else:
+    var=1
+    print(var)
